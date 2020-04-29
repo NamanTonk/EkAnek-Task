c796abfae6b96953ce36cd05eb568be4ee98c741
naman <naman@groomefy.com>
2020-04-30 01:49:20 +0530

    Create Task

--- app/src/main/java/com/e/ekanektask/Utils/RetroCall.kt
+++ app/src/main/java/com/e/ekanektask/Utils/RetroCall.kt
@@ -11,7 +11,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 import java.nio.charset.Charset
 import java.nio.charset.StandardCharsets
-
 class RetroCall {
     companion object {
         var CallAPi = Retrofit.Builder().baseUrl(String(Base64.decode( "aHR0cHM6Ly9waXhhYmF5LmNvbQ==",Base64.DEFAULT), Charset.forName("UTF-8"))).client(createIntercepter())
